2929e13c4ace32004d545ead1487c4b2a4b909db
Vivek Raj <118910972+vivekraj02@users.noreply.github.com>
2025-01-17 18:04:28 +0000

    var & datatype

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,22 @@
+const accountId = 1245
+let accountEmail = "raj@gamil.com"
+var accountPassword = 111
+accountCity = "Delhi"
+
+// accountId = 2 not allowed
+
+accountEmail = "hs@hddj"
+accountPassword = 1212
+accountCity = "Noida"
+let accountState
+console.log(accountId)
+
+/*
+Prefer not to use var
+because of issue in block scope and fuctional scope
+*/
+
+console.table([accountId , accountEmail , accountPassword , accountCity , accountState])
+
+
+
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict" // treat all JS code as newer version
+
+// alert(3+3) we are using nodejs not browser
+
+// code readibilty should be high
+
+let name = "raj"
+let age = 18
+let isLogin = true
+
+/*number => 2 to power 53
+bigint
+string  => " "
+boolean => true/false
+null => standalone value
+undefined => 
+symbol => for uniqueness 
+*/
+
+console.log(typeof null) //type is object
+console.log(typeof undefined) //type is undefined
